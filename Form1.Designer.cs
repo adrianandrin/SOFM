13e5db27f66da9a98b14a432807d9e1984eb619f
wiggle131 <jhosiecharlesespantaleon@gmail.com>
2021-02-24 16:01:38 +0800

    probable coordinates

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,34 +29,32 @@ namespace SOFM
         /// </summary>
         private void InitializeComponent()
         {
-            this.imageMagnifier1 = new ImageMagnifier.ImageMagnifier();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // imageMagnifier1
+            // button1
             // 
-            this.imageMagnifier1.ImageToMagnify = null;
-            this.imageMagnifier1.Location = new System.Drawing.Point(207, 125);
-            this.imageMagnifier1.MagnificationCoefficient = 1;
-            this.imageMagnifier1.Name = "imageMagnifier1";
-            this.imageMagnifier1.Size = new System.Drawing.Size(75, 23);
-            this.imageMagnifier1.TabIndex = 0;
-            this.imageMagnifier1.Text = "imageMagnifier1";
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.imageMagnifier1);
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
 
+
         #endregion
 
-        private ImageMagnifier.ImageMagnifier imageMagnifier1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
